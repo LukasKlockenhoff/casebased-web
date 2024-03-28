92047438fb92bf03646c231794807b3dcfdaa8fd
lukasklockenhoff <lukasklockenhoff@icloud.com>
2024-03-29 00:28:55 +0100

    feat:og

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "Case Based",
   url: "https://casebased.org/",
-  ogImage: "https://casebased.org/og.png",
+  ogImage: "https://dev.casebased.org/og.png",
   description:
     "Teaching and learning resources for case-based learning in computer science.",
   navigation: {
