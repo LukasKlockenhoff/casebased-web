55caae481227965a083438bab62f9f69061fb42f
lukasklockenhoff <lukasklockenhoff@icloud.com>
2024-03-28 18:09:19 +0100

    feat: logo and margins

--- src/app/roadmaps/page.tsx
+++ src/app/roadmaps/page.tsx
@@ -1,52 +1,92 @@
-import {Button} from "@/components/ui/button";
-import {CodeIcon} from "@radix-ui/react-icons";
-import {twMerge} from "tailwind-merge";
-import {textStyles} from "@/utils/textstyles";
+import { Button } from "@/components/ui/button";
+import { CodeIcon } from "@radix-ui/react-icons";
+import { twMerge } from "tailwind-merge";
+import { textStyles } from "@/utils/textstyles";
 import PipInstallSnipped from "@/components/home/pip-install-snipped";
-import {HomeGridCards} from "@/components/home/grid-cards";
-import {RoadmapsGridCards} from "@/components/roadmaps/roadmaps-grid-cards";
+import { HomeGridCards } from "@/components/home/grid-cards";
+import { RoadmapsGridCards } from "@/components/roadmaps/roadmaps-grid-cards";
 import Image from "next/image";
 import roadmapFoundationsLight from "@/assets/roadmaps-foundations-light.png";
 import roadmapKnowledgeLight from "@/assets/roadmaps-knowledge-light.png";
 import roadmapProductiveLight from "@/assets/roadmaps-productive-light.png";
 
 export default function Page() {
-    return (
-        <main className="flex w-full h-full flex-col items-center overflow-x-hidden bg-blur-roadmaps dark:bg-blur-dark-roadmaps">
-            <div className="container w-full flex flex-col mt-40 items-center gap-12">
-                <div className="flex flex-col items-center">
-                    <Button variant={"outline"} className={"bg-surface rounded-lg h-10 px-3 flex"}>
-                        <CodeIcon className={"w-5 h-5 mr-2"}/>
-                        <div className={twMerge(textStyles.muted, "mx-auto w-full")}>In Development Now!</div>
-                    </Button>
-                    <div className={twMerge(textStyles.gradient1, "text-center")}>
-                        Introducing Roadmaps
-                    </div>
-                    <div className={twMerge(textStyles.lead, "w-[90%] sm:w-1/2 text-center")}>
-                        <span className={twMerge(textStyles.lead, "italic")}>Roadmaps</span> is a tool to help you plan
-                        your learning path in Machine Learning.
-                        Check out our resources and examples to get started!
-                    </div>
-                    <div className="flex gap-4 mt-8">
-                        <Button variant={"default"}>Get Started</Button>
-                        <Button variant={"outline"}>Learn More</Button>
-                    </div>
-                </div>
-                <div className="flex flex-col items-center sm:grid gap-4 sm:grid-cols-3 md:gap-8">
-                    <RoadmapsGridCards href={"/"} title={"The CBR Cycle"}
-                                   description={"Learn about the foundations of Case Based Reasoning, how it works and where it can be applied."}>
-                        <Image src={roadmapFoundationsLight} width={348} height={200} alt={"The CBR Cycle"} className={"rounded"}/>
-                    </RoadmapsGridCards>
-                    <RoadmapsGridCards href={"/"} title={"Knowledge and Reasoning"}
-                                   description={"Apply the foundations of Case-Based Reasoning and build reasoning models from the construction of knowledge bases to solving emerging problems."}>
-                        <Image src={roadmapKnowledgeLight} width={348} height={200} alt={"The CBR Cycle"} className={"rounded-lg"}/>
-                    </RoadmapsGridCards>
-                    <RoadmapsGridCards href={"/"} title={"Productive with CBR"}
-                                   description={"Get creative with the tools of our Library and build your own applications that use Case-Based Reasoning to solve real world problems."}>
-                        <Image src={roadmapProductiveLight} width={348} height={200} alt={"The CBR Cycle"} className={"rounded-lg"}/>
-                    </RoadmapsGridCards>
-                </div>
+  return (
+    <main className="flex h-full w-full flex-col items-center overflow-x-hidden bg-blur-roadmaps dark:bg-blur-dark-roadmaps">
+      <div className="container mt-40 flex w-full flex-col items-center gap-12">
+        <div className="flex flex-col items-center">
+          <Button
+            variant={"outline"}
+            className={"flex h-10 rounded-lg bg-surface px-3"}
+          >
+            <CodeIcon className={"mr-2 h-5 w-5"} />
+            <div className={twMerge(textStyles.muted, "mx-auto w-full")}>
+              In Development Now!
             </div>
-        </main>
-    )
+          </Button>
+          <div className={twMerge(textStyles.gradient1, "text-center")}>
+            Introducing Roadmaps
+          </div>
+          <div
+            className={twMerge(textStyles.lead, "w-[90%] text-center sm:w-1/2")}
+          >
+            <span className={twMerge(textStyles.lead, "italic")}>Roadmaps</span>{" "}
+            is a tool to help you plan your learning path in Machine Learning.
+            Check out our resources and examples to get started!
+          </div>
+          <div className="mt-8 flex gap-4">
+            <Button variant={"default"}>Get Started</Button>
+            <Button variant={"outline"}>Learn More</Button>
+          </div>
+        </div>
+        <div className="mt-20 flex flex-col items-center gap-4 sm:grid sm:grid-cols-3 md:gap-8">
+          <RoadmapsGridCards
+            href={"/"}
+            title={"The CBR Cycle"}
+            description={
+              "Learn about the foundations of Case Based Reasoning, how it works and where it can be applied."
+            }
+          >
+            <Image
+              src={roadmapFoundationsLight}
+              width={348}
+              height={200}
+              alt={"The CBR Cycle"}
+              className={"rounded"}
+            />
+          </RoadmapsGridCards>
+          <RoadmapsGridCards
+            href={"/"}
+            title={"Knowledge and Reasoning"}
+            description={
+              "Apply the foundations of Case-Based Reasoning and build reasoning models from the construction of knowledge bases to solving emerging problems."
+            }
+          >
+            <Image
+              src={roadmapKnowledgeLight}
+              width={348}
+              height={200}
+              alt={"The CBR Cycle"}
+              className={"rounded-lg"}
+            />
+          </RoadmapsGridCards>
+          <RoadmapsGridCards
+            href={"/"}
+            title={"Productive with CBR"}
+            description={
+              "Get creative with the tools of our Library and build your own applications that use Case-Based Reasoning to solve real world problems."
+            }
+          >
+            <Image
+              src={roadmapProductiveLight}
+              width={348}
+              height={200}
+              alt={"The CBR Cycle"}
+              className={"rounded-lg"}
+            />
+          </RoadmapsGridCards>
+        </div>
+      </div>
+    </main>
+  );
 }
--- src/components/ui/main-nav.tsx
+++ src/components/ui/main-nav.tsx
@@ -1,54 +1,56 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import Link from "next/link"
-import { usePathname } from "next/navigation"
+import * as React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-import { siteConfig } from "@/config/site"
-import { twMerge } from "tailwind-merge"
-import { Icons } from "@/components/ui/icons"
+import { siteConfig } from "@/config/site";
+import { twMerge } from "tailwind-merge";
+import { Icons } from "@/components/ui/icons";
+import { useTheme } from "@geist-ui/core";
 
 export function MainNav() {
-    const pathname = usePathname()
+  const pathname = usePathname();
+  const theme = useTheme();
 
-    return (
-        <div className="mr-4 hidden md:flex">
-            <Link href="/" className="mr-6 flex items-center space-x-2">
-                <Icons.logo className="h-6 w-6" />
-                <span className="hidden font-bold sm:inline-block">
+  return (
+    <div className="mr-4 hidden md:flex">
+      <Link href="/" className="mr-6 flex items-center space-x-2">
+        <Icons.logo className="h-6 w-6" />
+        <span className="hidden font-bold sm:inline-block">
           {siteConfig.name}
         </span>
-            </Link>
-            <nav className="flex items-center gap-4 text-sm lg:gap-6">
-                <Link
-                    href={siteConfig.navigation.docs}
-                    className={twMerge(
-                        "transition-colors hover:text-foreground/80",
-                        pathname === "/docs" ? "text-foreground" : "text-foreground/60"
-                    )}
-                >
-                    Docs
-                </Link>
-                <Link
-                    href={siteConfig.navigation.examples}
-                    className={twMerge(
-                        "transition-colors hover:text-foreground/80",
-                        pathname?.startsWith("/examples")
-                            ? "text-foreground"
-                            : "text-foreground/60"
-                    )}
-                >
-                    Examples
-                </Link>
-                <Link
-                    href={siteConfig.navigation.roadmaps}
-                    className={twMerge(
-                        "hidden text-foreground/60 transition-colors hover:text-foreground/80 lg:block"
-                    )}
-                >
-                    Roadmaps
-                </Link>
-            </nav>
-        </div>
-    )
+      </Link>
+      <nav className="flex items-center gap-4 text-sm lg:gap-6">
+        <Link
+          href={siteConfig.navigation.docs}
+          className={twMerge(
+            "hover:text-foreground/80 transition-colors",
+            pathname === "/docs" ? "text-foreground" : "text-foreground/60",
+          )}
+        >
+          Docs
+        </Link>
+        <Link
+          href={siteConfig.navigation.examples}
+          className={twMerge(
+            "hover:text-foreground/80 transition-colors",
+            pathname?.startsWith("/examples")
+              ? "text-foreground"
+              : "text-foreground/60",
+          )}
+        >
+          Examples
+        </Link>
+        <Link
+          href={siteConfig.navigation.roadmaps}
+          className={twMerge(
+            "text-foreground/60 hover:text-foreground/80 hidden transition-colors lg:block",
+          )}
+        >
+          Roadmaps
+        </Link>
+      </nav>
+    </div>
+  );
 }
